76cb0235b916d99cf9534be5e904ddd1e78d0534
Shunichi09 <37980935+Shunichi09@users.noreply.github.com>
2018-07-26 23:30:56 +0900

    add

--- Static/Template/template.py
+++ Static/Template/template.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import sys
-import math
 import statsmodels.formula.api as smf
 import statsmodels.api as sm 
 from sklearn import linear_model
@@ -30,19 +28,32 @@ np.corrcoef(np_data, rowvar=False)
 # 回帰
 # sklearnの場合
 # 説明変数の列抜き出し
-X = np_data[:, 'input your rows']
+X = np_data[:, 'input your column']
 
 # 目的変数の列抜き出し
-Y = np_data[:, 'input your rows']
+Y = np_data[:, 'input your column']
 
 # 結果出してくれる（切片とか正規化とかその他もろもろ設定可能）
 clf = linear_model.LinearRegression(fit_intercept=True, normalize=False, copy_X=True)
  
 # 予測モデルを作成
-clf.fit(X, Y)
-clf.coef_(X, Y)
-clf.score(X, Y)# 
-clf.predict(X)#予測してくれる 説明変数が入力
+clf.fit(X, Y) # これでfit（学習）させる 
+clf.coef_(X, Y) # 偏回帰係数を出してくれる  
+clf.score(X, Y) # 決定係数を出してくれる
+clf.predict(X) # 予測してくれる 説明変数が入力
 
 # statsmodelの場合
+# 説明変数の列抜き出し
+X = np_data[:, 'input your column']
+
+# 目的変数の列抜き出し
+Y = np_data[:, 'input your column']
+
+X_y = sm.add_constant(X) # 切片を追加
+ 
+model = smf.OLS(Y, X)# 逆なので注意！
+
+result = model.fit()# これで結果が見える
+
+print(result.summary())
 
