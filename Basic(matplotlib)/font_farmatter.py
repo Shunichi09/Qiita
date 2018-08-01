ee8e46f514f5b2aa49ed2a64cce950d97a1748ca
Shunichi09 <37980935+Shunichi09@users.noreply.github.com>
2018-08-01 23:54:17 +0900

    add

---
+++ Basic(matplotlib)/font_farmatter.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# defaultの設定
+plt.rcParams['font.family'] = 'Times New Roman' #全体のフォントを設定
+plt.rcParams['font.size'] = 15 #フォントサイズを設定
+plt.rcParams['lines.linewidth'] = 1.5
+
+# その他の設定(tex)の表記
+(r'$\mathit{x}$ [m]')
+
+# 下記でtex表記を変更できる
+# plt.rcParams["mathtext.fontset"] = "stix"
+plt.rcParams['font.family'] = 'Times New Roman'  #全体のフォントを設定"STIXGeneral" 'serif'
+# plt.rcParams["mathtext.fontset"] = "dejavusans"  #全体のフォントを設定 "cm","stix","dejavusans","custom","dejavusans"
+# plt.rcParams["mathtext.rm"] = "Times New Roman"
